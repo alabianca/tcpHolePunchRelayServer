d576f19ed92ec734d79388c1c329d90db46bec05
Alexander <labianca.alex@gmail.com>
2019-01-04 14:00:08 -0600

    conn response/request

--- udp.js
+++ udp.js
@@ -4,6 +4,8 @@ const dgram = require('dgram')
 const socket = dgram.createSocket("udp4")
 const CREATE_SESSION = 0x3C
 const CREATE_SESSION_RESPONSE = 0x3D
+const CONN_REQ = 0x3E
+const CONN_REQ_RESPONSE = 0x3F
 const ACK = 6
 const NAK = 21
 
@@ -34,6 +36,13 @@ function handleMessage(socket,msg,rinfo) {
             const ok = storeConnection(msg,rinfo)
             const response = ok ? createSessionResponsePacket(ACK) : createSessionResponsePacket(NAK);
             socket.send(response, rinfo.port, rinfo.address)
+            break;
+        
+        case CONN_REQ:
+            const conn = parseConnectionRequestPacket(msg);
+            const response = conn ? createConnectionResponsePacket(ACK) : createConnectionResponsePacket(NAK);
+            socket.send(response,rinfo.port,rinfo.address);
+            break;
     }
 }
 
@@ -56,6 +65,51 @@ function storeConnection(msg,rinfo) {
 function createSessionResponsePacket(AckNak) {
     const buf = Buffer.from([CREATE_SESSION_RESPONSE, AckNak])
     return buf
+}
+
+function parseConnectionRequestPacket(packet) {
+    const user = packet.slice(1);
+
+    if(connections[user]) {
+        return null
+    }
+
+    return connections[user]
+}
+
+function createConnectionResponsePacket(AckNak,connection) {
+    const start = Buffer.from([CONN_REQ_RESPONSE,AckNak])
+
+    if(!connection) {
+        return start
+    }
+    //encode the port
+    const larr = new Uint16Array(1)
+    larr[0] = parseInt(connection.port)
+    const port = Buffer.from(larr.buffer).swap16()
+    //encode the ip
+    const ipBytes = connection.family == "IPv4" ? encodeIpv4(connection.address) : encodeIpv6(connection.address);
+
+    const ip = Buffer.from(ipBytes)
+    
+    return Buffer.concat([start,ip,port])
+}
+
+function encodeIpv4(ipv4) {
+   return ipv4.split(".").map(byte => parseInt(byte))
+}
+
+function encodeIpv6(ipv6) {
+    const ipBytes = []
+    const split = ipv6.split(":")
+    for(let i = 0; i < split.length; i++) {
+        const lower = split[i].substring(0,2)
+        const higher = split[i].substring(2)
+        ipBytes.push(parseInt(lower,16))
+        ipBytes.push(parseInt(higher,16))
+    }
+
+    return ipBytes
 }
 
 socket.bind(41234)
